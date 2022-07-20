024537f5f5f0ba2652da881134e05bf9dff29e8a
jmindevelper <jmin.developer@gmail.com>
2022-07-20 16:46:51 +0900

    DiffableDataSource 기본기능 구현
    - UITableViewDefaubleDataSource
    - NSDiffableDataSourceSnapshot
    - 추가, 삭제 버튼 구현

--- DiffableDataSource/DiffableDataSource/ViewController.swift
+++ DiffableDataSource/DiffableDataSource/ViewController.swift
@@ -7,13 +7,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Item: Hashable {
+    let id = UUID().uuidString
+    let content: String
+    
+    init(content: String) {
+        self.content = content
+    }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController {
+    
+    enum TableViewSection: CaseIterable {
+        case main
     }
 
+    // MARK: - Outlet
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Properties
+    private var dataSource: UITableViewDiffableDataSource<TableViewSection, Item>!
+    private var tableViewItem = [Item]()
+    private let randomItemContent = ["김지민", "김은서", "김현식", "뽀로로"]
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureDiffableDataSource()
+    }
 
+    // MARK: - Method
+    private func configureDiffableDataSource() {
+        dataSource = UITableViewDiffableDataSource<TableViewSection, Item>(tableView: tableView) { table, indexPath, item -> UITableViewCell? in
+            guard let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? Cell else { return UITableViewCell() }
+            cell.label.text = item.content
+            return cell
+        }
+    }
+    
+    private func applySnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<TableViewSection, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(tableViewItem)
+        self.dataSource.apply(snapshot, animatingDifferences: true)
+    }
+    
+    // MARK: - Action
+    @IBAction func addTableViewItemButton(_ sender: Any) {
+        let item = Item(content: randomItemContent.randomElement() ?? "김지민")
+        tableViewItem.append(item)
+        applySnapshot()
+    }
+    
+    @IBAction func deleteTableViewItemButton(_ sender: Any) {
+        tableViewItem.removeLast()
+        applySnapshot()
+    }
+}
+
+class Cell: UITableViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
 }
 
